bac88c4af9461b7e1c5a5ec7bc78bf8e3188790e
Piotr <piotr.ostrowski@aol.com>
2022-03-30 20:54:37 +0200

    fmt

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region     = "us-east-2"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,20 @@
 variable "AWS_ACCESS_KEY" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "AWS_SECRET_KEY" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "SSH_PASSWORD" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "DL_AMI" {
-  type = string
+  type      = string
   sensitive = false
-  default = "ami-0fd4336c5e2ccb32d"
+  default   = "ami-0fd4336c5e2ccb32d"
 }
