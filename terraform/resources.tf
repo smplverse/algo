af7909921ec174f4d311682fe51b293b0caf0bdd
Piotr <piotr.ostrowski@aol.com>
2022-03-30 14:41:40 +0200

    update resources, boot

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,31 +1,36 @@
-resource "aws_instance" "foo" {
+resource "aws_instance" "smplverse_instance" {
   ami           = var.DL_AMI
   instance_type = "g4dn.xlarge"
   count         = 1
-  key_name      = "smplverse_key"
+  key_name      = "smplverse"
 
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  vpc_security_group_ids = [aws_security_group.smplverse_sg.id]
 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    host        = self.public_ip
     private_key = file("~/.ssh/smplverse")
+    timeout     = "10s"
   }
 }
 
-resource "aws_security_group" "sg" {
-  ingress {
-      protocol  = "tcp"
-      from_port = 22
-      to_port   = 22
-  }
-
+resource "aws_security_group" "smplverse_sg" {
   egress {
+    cidr_blocks      = ["0.0.0.0/0"]
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
+
+  ingress {
+    cidr_blocks      = ["0.0.0.0/0"]
+    protocol         = "tcp"
+    from_port        = 22
+    to_port          = 22
+  }
+}
+
+resource "aws_key_pair" "smplverse_key" {
+  key_name = "smplverse"
+  public_key = file("~/.ssh/smplverse.pub")
 }
