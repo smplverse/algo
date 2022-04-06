a66e9c4b6f0aae3a0d48ac294bea5e240990f309
Piotr <piotr.ostrowski@aol.com>
2022-04-06 16:28:26 +0200

    add profile, share volume with container not to download models every time

--- src/profiler.py
+++ src/profiler.py
@@ -1,22 +1,25 @@
+import tensorflow as tf
+
 from deepface import DeepFace
 from model_profiler import model_profiler
 
 
 def run_profiler():
-    model_names = [
-        'VGG-Face',
-        'OpenFace',
-        'Facenet',
-        'Facenet512',
-        'DeepFace',
-        'DeepID',
-        'ArcFace',
-    ]
-    for model_name in model_names:
-        model = DeepFace.build_model(model_name)
-        profile = model_profiler(model, 1)
-        print("model_name:", model_name)
-        print(profile, "\n")
+    with tf.device("CPU"):
+        model_names = [
+            'VGG-Face',
+            'OpenFace',
+            'Facenet',
+            'Facenet512',
+            'DeepFace',
+            'DeepID',
+            'ArcFace',
+        ]
+        for model_name in model_names:
+            model = DeepFace.build_model(model_name)
+            profile = model_profiler(model, 1)
+            print("model_name:", model_name)
+            print(profile, "\n")
 
 
 if __name__ == "__main__":
