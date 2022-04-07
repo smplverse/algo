7e390673c953d669e0e972f0be00052441c7a327
Piotr <piotr.ostrowski@aol.com>
2022-04-07 18:37:30 +0200

    name inputs and outputs of onnx export

--- src/export_to_onnx.py
+++ src/export_to_onnx.py
@@ -8,12 +8,18 @@ from facenet_pytorch import InceptionResnetV1
 def export_to_onnx(model: torch.nn.Module, model_name: str):
     # most have the same input size (224, 224)
     torch_model = model.cuda().eval()
-    dummy_input = torch.randn(10, 3, 224, 224, device="cuda")
+    dummy_input = torch.randn(1, 3, 224, 224, device="cuda")
     onnx_model_name = f"{model_name}.onnx"
     if onnx_model_name in os.listdir('models'):
         print("onnx model already exists")
         return
-    torch.onnx.export(torch_model, dummy_input, f"models/{onnx_model_name}")
+    torch.onnx.export(
+        torch_model,
+        dummy_input,
+        f"models/{onnx_model_name}",
+        input_names=["input_1"],
+        output_names=["output_1"],
+    )
     onnx_model = onnx.load(f"models/{onnx_model_name}")
     try:
         onnx.checker.check_model(onnx_model)
